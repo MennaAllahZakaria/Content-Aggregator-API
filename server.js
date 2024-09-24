1f1f8b3f7162475ccc5962261bb6ac077ec52cca
MennaAllahZakaria <mennazakaria2003@gmail.com>
2024-09-24 20:14:35 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const path=require('path');
+
+const express=require("express");
+const cors=require('cors');
+const compression=require('compression');
+
+const dotenv=require("dotenv");
+const morgan=require("morgan");
+
+const rateLimit=require('express-rate-limit');
+const hpp=require('hpp')
+const mongoSanitize=require('express-mongo-sanitize');
+const xss= require('xss-clean');
+
+dotenv.config({path:"config.env"});
+
+const dbConnection=require('./config/database');
+const ApiError=require("./utils/ApiError");
+const globalError=require('./middlewares/errorMiddleware');
+
+const mountRoutes=require('./routes/index')
+
+//exress app
+const app=express();
+//cors -> other domains can access our app
+app.use(cors());
+app.options('*',cors());
+
+//compression -> compress all responses
+app.use(compression());
+
+// limit repeated requests to public APIs and/or endpoints
+app.use(express.json({limit:'20kb'}));
+app.use(express.static(path.join(__dirname, 'uploads')));
+
+//To apply data sanitization
+app,use(mongoSanitize());
+
+if ( process.env.NODE_ENV==='development'){
+    app.use(morgan('dev'));
+    console.log(`mode : ${process.env.NODE_ENV}`);
+}
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, 
+    message:'Too many requests,please try again after an hour'
+});
+
+// Apply the rate limiting middleware to all requests.
+app.use("/api",limiter);
+
+//Mount route
+mountRoutes(app);
+
+app.all('*',(req,res,next)=>{
+    // create error and send it to error handling middleware
+        next(new ApiError(`cannot find this route : ${req.originalUrl}`,400))
+    });
+
+    const PORT=process.env.PORT|| 8000;
+
+    const server= app.listen(PORT,()=>{
+        console.log(`App Running on port ${PORT}`);
+    });
+    
+    // Handle Rejections outside express
+    process.on("unhandledRejection",(err)=>{
+        console.log(`UnhandledRejection Errors: ${err}`);
+        server.close(()=>{
+            console.error('Shutting Down...');
+            process.exit(1);
+        })
+        
+    })
